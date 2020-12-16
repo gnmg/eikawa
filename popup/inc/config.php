3cd64036e5ec7950e4fc4a42791243f045fedcc0
gnmg <ralph@j-start.jp>
2020-12-16 15:40:53 +0900

    initial commit

---
+++ popup/inc/config.php
@@ -0,0 +1,8 @@
+<?php
+define('UAP_DB_HOST', 'localhost');
+define('UAP_DB_HOST_PORT', '');
+define('UAP_DB_USER', 'eikaiwa_gnmg');
+define('UAP_DB_PASSWORD', 'igrcnmt2igrcnmt2');
+define('UAP_DB_NAME', 'eikaiwa_popup');
+define('UAP_TABLE_PREFIX', 'uap_');
+?>
